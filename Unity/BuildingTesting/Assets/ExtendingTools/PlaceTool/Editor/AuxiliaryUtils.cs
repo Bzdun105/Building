ba825df941cc16c47abca2e8a0d03b163acd8b1d
Zhivaev Dmitry <zhivaevdmitry@gmail.com>
2016-11-11 20:03:19 +0300

    Instantiator

---
+++ Unity/BuildingTesting/Assets/ExtendingTools/PlaceTool/Editor/AuxiliaryUtils.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+//#if UNITY_EDITOR
+using UnityEditor;
+//#endif
+
+using System.Collections.Generic;
+
+namespace ExtendingTools.PlaceTool
+{
+	/*
+	public static class AuxiliaryUtils
+	{
+
+	}
+
+	public class Tool
+	{
+		private GameObject _instance_object;
+	}
+
+	//#if UNITY_EDITOR
+	public class PlaceAtSelectedTransformsTool
+	{
+		public enum ErrorMessage
+		{
+			NoInstance,
+			TransformsNotSelected
+		}
+
+		public enum HierarchyPlace
+		{
+			Replacement = 0,
+			Child			= 1,
+			Sibling		= 2
+		}
+
+		public PlaceAtSelectedTransformsTool()
+		{
+			Selection.selectionChanged += 
+		}
+
+		public void OnEnable()
+		{
+		}
+
+		public GameObject					InstanceToPlace		{ get; set; }
+		public HierarchyPlace			PlaceAsToTransform	{ get; set; }
+
+		public List<ErrorMessage> ErrorMessages
+		{
+			get
+			{ 
+				List<ErrorMessage> messages_set = new List<ErrorMessage>();
+
+				if (InstanceToPlace == null)				messages_set.Add(ErrorMessage.NoInstance);
+				if (Selection.transforms.Length == 0)	messages_set.Add(ErrorMessage.TransformsNotSelected);
+
+				return messages_set;
+			}
+		}
+
+		
+		//public delegate void itemSelectedDelegate(PaletteItem item, Texture2D preview);
+		//public static event itemSelectedDelegate ItemSelectedEvent;
+	}
+	//#endif
+
+	public class PlaceAtGrid
+	{
+		public GameObject				InstanceToPlace			{ get; set; }
+	}
+
+	public static class AuxiliaryUtils
+	{
+
+	}
+	*/
+}
